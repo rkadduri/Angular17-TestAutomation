1de717ec5c198a3f15c7134188473dae935fd8a5
KatyayaniDeviGatti <gattikatyayanidevi@gmail.com>
2024-04-29 13:09:13 +0530

    commit selectbiller if statements

--- src/app/dashboard/payments/payment-success/payment-success.component.ts
+++ src/app/dashboard/payments/payment-success/payment-success.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../account-details/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { BankingdataService } from '../../../bankingdata.service';
@@ -7,12 +7,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-payment-success',
   standalone: true,
-  imports: [NavbarComponent,CommonModule,RouterOutlet,RouterLink],
+  imports: [NavbarComponent,CommonModule,RouterLink],
   providers:[BankingdataService],
   templateUrl: './payment-success.component.html',
   styleUrl: './payment-success.component.css'
 })
-export class PaymentSuccessComponent {
+export class PaymentSuccessComponent{
 
   selectBillerPayment:any;
   mobileRechargePayment:any
@@ -20,8 +20,13 @@ export class PaymentSuccessComponent {
   constructor(private service:BankingdataService){}
   recharge:boolean = this.service.rechargePaymentSuccess
   ngOnInit(){
+    console.log(this.service.selectBillerSuccess)
     this.selectBillerPayment =  this.service.selectBillerSuccess;
     this.mobileRechargePayment = this.service.selectBillerSuccess
     console.log(this.selectBillerPayment)
+    console.log(this.mobileRechargePayment)
   }
+
+  
+
 }
