bb1ddb579424a6a72a6ef5fb42f98f5ba00aca99
Sublari Leela Deepak <leeladeepak123@gmail.com>
2024-04-29 11:11:44 +0530

    transfer changes

--- src/app/dashboard/fund-transfer/fund-transfer.component.spec.ts
+++ src/app/dashboard/fund-transfer/fund-transfer.component.spec.ts
@@ -24,4 +24,32 @@ describe('FundTransferComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render paragraph with correct text and class for Payment method', () => {
+    const paragraphElement = fixture.debugElement.query(By.css('#paymentMethod'));
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement.nativeElement.textContent.trim()).toBe('Payment method');
+    expect(paragraphElement.nativeElement.getAttribute('class')).toContain('card-text');
+  });
+
+  it('should render paragraph with correct text and class for Add Payee', () => {
+    const paragraphElement = fixture.debugElement.query(By.css('#addPayee'));
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement.nativeElement.textContent.trim()).toBe('Add Payee');
+    expect(paragraphElement.nativeElement.getAttribute('class')).toContain('card-text');
+  });
+
+  it('should render paragraph with correct text and class for Manage Payee', () => {
+    const paragraphElement = fixture.debugElement.query(By.css('#managePayee'));
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement.nativeElement.textContent.trim()).toBe('Manage Payee');
+    expect(paragraphElement.nativeElement.getAttribute('class')).toContain('card-text');
+  });
+
+  it('should render paragraph with correct text and class for Statements', () => {
+    const paragraphElement = fixture.debugElement.query(By.css('#statements'));
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement.nativeElement.textContent.trim()).toBe('Statements');
+    expect(paragraphElement.nativeElement.getAttribute('class')).toContain('card-text');
+  });
 });
--- src/app/dashboard/fund-transfer/transfer-account-summary/transfer-account-summary.component.spec.ts
+++ src/app/dashboard/fund-transfer/transfer-account-summary/transfer-account-summary.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TransferAccountSummaryComponent } from './transfer-account-summary.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BankingdataService } from '../../../bankingdata.service';
 
 describe('TransferAccountSummaryComponent', () => {
   let component: TransferAccountSummaryComponent;
   let fixture: ComponentFixture<TransferAccountSummaryComponent>;
+  let service : BankingdataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TransferAccountSummaryComponent]
+      declarations: [TransferAccountSummaryComponent],
+      imports: [HttpClientModule],
+      providers: [BankingdataService]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(TransferAccountSummaryComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(BankingdataService)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('checking whether accountHolderName tag has expected textContent',()=>{
+    let accountHolderName = fixture.debugElement.nativeElement.querySelector('#accountHolderName');
+    let name = service.trimmedString;
+    expect(accountHolderName.textContent).toEqual(name)
+  });
+
+  it('checking whether accountType has expected textContent',()=>{
+    let accountType = fixture.debugElement.nativeElement.querySelector('#accountType');
+    expect(accountType.textContent).toEqual('Savings Account')
+  });
+
+  it('checking whether accountNumber has expected textContent',()=>{
+    let accountNumber = fixture.debugElement.nativeElement.querySelector('#accountNumber');
+    expect(accountNumber.textContent).toEqual('1234567890111213')
+  });
+
+  it('checking whether ifscCode has expected textContent',()=>{
+   let ifscCode = fixture.debugElement.nativeElement.querySelector('#ifscCode');
+   expect(ifscCode.textContent).toEqual('ABCD0001234')
+  });
+
+  it('checking whether available balance has expected value',()=>{
+    let balance = fixture.debugElement.nativeElement.querySelector('#balance');
+    let balanceText = balance.textContent.trim();
+    let numericBalance = parseFloat(balanceText.replace(/[^\d.-]/g, ''));
+    let balanceValue = service.balance;
+    expect(numericBalance).toEqual(balanceValue)
+  });
+
+  it('checking whether branch has expected textContent',()=>{
+    let branch = fixture.debugElement.nativeElement.querySelector('#branch');
+    expect(branch.textContent).toEqual('KPHB')
   });
 });
